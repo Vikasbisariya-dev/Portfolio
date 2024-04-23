5f6e3ed78098177528faa877cb44c5fd0c9188ea
vikas bisariya <vikasbisariya20@gmail.com>
2024-04-23 19:39:05 +0530

    resourses

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter } from "react-router-dom";
+import  {About,Contact,Hero,Navbar,Works,Experience,Feedbacks,Tech, StarsCanvas} from './components';
+
+function App() {
+  
+
+  return (
+    <BrowserRouter>
+       <div className="relaive z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar/>
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+        <Contact />
+        <StarsCanvas />
+        </div>
+       </div>
+    </BrowserRouter>
+  )
+}
+
+export default App
